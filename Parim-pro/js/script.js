bd9bca6976db77a8933c08d893b45e0fd677f7a1
chinemerem-dan <obinnaowunna8@gmail.com>
2026-01-16 12:38:52 +0100

    new file added

---
+++ Parim-pro/js/script.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.querySelector(".menu-toggle");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", () => {
+  window.location.href = "dashboard.html";
+});
